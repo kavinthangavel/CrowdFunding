c3da725250ab8e654512db9c6fa4d62967af9620
kavinthangavel <tdskavinkalkin@gmail.com>
2024-02-09 03:16:50 +0530

    Added Web3Auth

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,39 @@ import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(), nodePolyfills()],
+//   define: {
+//     "process.env": {},
+//   },
+// });
+
+
+/* eslint-disable import/no-extraneous-dependencies */
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      crypto: "empty-module",
+      assert: "empty-module",
+      http: "empty-module",
+      https: "empty-module",
+      os: "empty-module",
+      url: "empty-module",
+      zlib: "empty-module",
+      stream: "empty-module",
+      _stream_duplex: "empty-module",
+      _stream_passthrough: "empty-module",
+      _stream_readable: "empty-module",
+      _stream_writable: "empty-module",
+      _stream_transform: "empty-module",
+    },
+  },
   define: {
-    "process.env": {},
+    global: "globalThis",
   },
 });
